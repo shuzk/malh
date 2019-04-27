78e59761fb45e1b47fe2fc66e78f487d86acf675
shuzk <15602462917@163.com>
2019-04-27 20:58:23 +0800

    03_导入数据库扩展，并在配置中填写相关配置

--- manage.py
+++ manage.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 
 class Config(object):
     """工程配置信息"""
+
     DEBUG = True
+
+    # 数据库配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/malhflasksql"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 app.config.from_object(Config)
+db = SQLAlchemy(app)
